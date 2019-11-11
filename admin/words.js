d3db9362e8e30bad98e9781a5e6049ca7c7cba5d
cmoehler <ccm@gmx.netgit config --global user.email ccm@gmx.netgit config --global user.email ccm@gmx.net>
2019-11-11 13:40:16 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'ocontrol adapter settings': {
+		'en': 'Adapter settings for ocontrol',
+		'de': 'Adaptereinstellungen für ocontrol',
+		'ru': 'Настройки адаптера для ocontrol',
+		'pt': 'Configurações do adaptador para ocontrol',
+		'nl': 'Adapterinstellingen voor ocontrol',
+		'fr': "Paramètres d'adaptateur pour ocontrol",
+		'it': "Impostazioni dell'adattatore per ocontrol",
+		'es': 'Ajustes del adaptador para ocontrol',
+		'pl': 'Ustawienia adaptera dla ocontrol',
+		'zh-cn': 'ocontrol的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};
